6ebe1a87a77a412a16a54414e89d8c286e3744a1
LyanKaleu <lyankaleu12@gmail.com>
2024-04-06 10:06:16 -0300

    Finalizando a atividade de encapsulamento:
    Atividade 05/04/2024 - Encapsulamento: Classe Pessoa

--- ATIVIDADE - 05-04-2024.py
+++ ATIVIDADE - 05-04-2024.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    # CONSTRUTOR (3 PARÂMETROS OPCIONAIS)
     def __init__(self, nome, idade, peso, altura, sexo, estado=True, estado_civil='solteiro(a)', conjuge=None):
         self.nome = nome
         self.__idade = idade
@@ -9,15 +10,16 @@ class Pessoa:
         self.__estado_civil = estado_civil
         self.__conjuge = conjuge
 
+    # DECORATORS / GETTERS E SETTERS (ENCAPSULAMENTO)
     @property
     def idade(self):
         if self.__estado:
             return f"{self.nome} possui {self.__idade} anos"
         else:
             if self.__sexo == 'M':
-                print(f"{self.nome} está morto")
+                return f"{self.nome} está morto"
             elif self.__sexo == 'F':
-                print(f"{self.nome} está morta")
+                return f"{self.nome} está morta"
 
     @idade.setter
     def idade(self, valor):
@@ -29,9 +31,9 @@ class Pessoa:
             return f"{self.nome} possui {self.__peso}Kg"
         else:
             if self.__sexo == 'M':
-                print(f"{self.nome} está morto")
+                return f"{self.nome} está morto"
             elif self.__sexo == 'F':
-                print(f"{self.nome} está morta")
+                return f"{self.nome} está morta"
 
     @peso.setter
     def peso(self, valor):
@@ -43,9 +45,9 @@ class Pessoa:
             return f"{self.nome} é do sexo {self.__sexo}"
         else:
             if self.__sexo == 'M':
-                print(f"{self.nome} está morto")
+                return f"{self.nome} está morto"
             elif self.__sexo == 'F':
-                print(f"{self.nome} está morta")
+                return f"{self.nome} está morta"
 
     @sexo.setter
     def sexo(self, valor):
@@ -71,9 +73,9 @@ class Pessoa:
                 return f"{self.nome} é {self.__estado_civil}"
         else:
             if self.__sexo == 'M':
-                print(f"{self.nome} está morto")
+                return f"{self.nome} está morto"
             elif self.__sexo == 'F':
-                print(f"{self.nome} está morta")
+                return f"{self.nome} está morta"
 
     @property
     def conjuge(self):
@@ -84,14 +86,15 @@ class Pessoa:
                 return f"{self.nome} não é casado(a)"
         else:
             if self.__sexo == 'M':
-                print(f"{self.nome} está morto")
+                return f"{self.nome} está morto"
             elif self.__sexo == 'F':
-                print(f"{self.nome} está morta")
+                return f"{self.nome} está morta"
 
     @conjuge.setter
     def conjuge(self, valor):
         print("Sem permissão")
 
+    # MÉTODOS PRINCIPAIS
     def envelhecer(self):
         if self.__estado:
             self.__idade += 1
@@ -132,28 +135,37 @@ class Pessoa:
             print(f"{self.nome} não pode mais crescer pois está com 21 anos ou mais")
 
     def casar(self, conjuge):
-        if conjuge.__idade > 18:
-            if ((self.__estado_civil == 'solteiro(a)' or self.__estado_civil == 'viúvo(a)')
-                    and (conjuge.estado_civil == 'solteiro(a)' or conjuge.estado_civil == 'viúvo(a)')):
-                self.__estado_civil = 'casado(a)'
-                self.__conjuge = conjuge
-                conjuge.estado_civil = 'casado(a)'
-                conjuge.conjuge = self.nome
-                if self.__sexo == 'M':
-                    print(f"{self.nome} está casado com {conjuge.nome}")
-                elif self.__sexo == 'F':
-                    print(f"{self.nome} está casada com {conjuge.nome}")
+        if self.__estado:
+            if conjuge.__idade >= 18:
+                if ((self.__estado_civil == 'solteiro(a)' or self.__estado_civil == 'viúvo(a)')
+                        and (conjuge.__estado_civil == 'solteiro(a)' or conjuge.__estado_civil == 'viúvo(a)')):
+                    self.__estado_civil = 'casado(a)'
+                    self.__conjuge = conjuge
+                    conjuge.__estado_civil = 'casado(a)'
+                    conjuge.__conjuge = self.nome
+                    if self.__sexo == 'M':
+                        print(f"{self.nome} está casado com {conjuge.nome}")
+                    elif self.__sexo == 'F':
+                        print(f"{self.nome} está casada com {conjuge.nome}")
+                else:
+                    if self.__sexo == 'M':
+                        print(f"Casamento não realizado. {self.nome} é casado.")
+                    elif self.__sexo == 'F':
+                        print(f"Casamento não realizado. {self.nome} é casada.")
             else:
-                if self.__sexo == 'M':
-                    print(f"Casamento não realizado. {self.nome} é casado.")
-                elif self.__sexo == 'F':
-                    print(f"Casamento não realizado. {self.nome} é casada.")
+                print(f"Casamento não permitido. {conjuge.nome} é de menor")
         else:
-            print(f"Casamento não permitido. {conjuge} é de menor")
+            if self.__sexo == 'M':
+                print(f"Casamento não realizado. {self.nome} está morto")
+            elif self.__sexo == 'F':
+                print(f"Casamento não realizado. {self.nome} está morta")
 
     def morrer(self):
         if self.__estado:
             self.__estado = False
+            if self.__conjuge:
+                self.__conjuge.__estado_civil = 'viúvo(a)'
+                self.__conjuge.__conjuge = None
             print(f"{self.nome} morreu")
         else:
             if self.__sexo == 'M':
@@ -162,7 +174,9 @@ class Pessoa:
                 print(f"{self.nome} já está morta")
 
 
+# FUNÇÃO MAIN (USADA PARA TESTAR E DEPURAR O CÓDIGO, CRIANDO INSTÂNCIAS DE OBJETOS)
 def main():
+    # CRIANDO PESSOAS (OBJETOS)
     maria = Pessoa("Maria", 5, 20, 100, 'F')
     joao = Pessoa("João", 12, 40, 140, 'M')
     pedro = Pessoa("Pedro", 22, 65, 170, 'M')
@@ -170,6 +184,24 @@ def main():
     julia = Pessoa("Júlia", 30, 65, 170, 'F')
     carlos = Pessoa("Carlos", 2, 11, 80, 'M')
     jonas = Pessoa("Jonas", 34, 70, 180, 'M')
+    
+    # TESTANDO MÉTODOS (COMPORTAMENTOS)
+    maria.idade = 10
+    maria.envelhecer()
+    pedro.crescer(2)
+    bia.casar(carlos)
+    pedro.casar(maria)
+    pedro.casar(julia)
+    pedro.casar(bia)
+    maria.morrer()
+    maria.engordar(10)
+    bia.casar(jonas)
+    bia.morrer()
+    pedro.morrer()
+    jonas.casar(julia)
+    pedro.casar(bia)
+    print(pedro.idade)
+    joao.idade = 50
 
 
 if __name__ == '__main__':
